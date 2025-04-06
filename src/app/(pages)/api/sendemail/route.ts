0ad29611f78da1ffef655ef2abf2b89e35c6a764
Ritesh <riteshmaharjan07@gmail.com>
2025-04-05 20:06:13 -0700

    Port 465

--- src/app/(pages)/api/sendemail/route.ts
+++ src/app/(pages)/api/sendemail/route.ts
@@ -7,9 +7,7 @@ export async function POST(req: Request) {
 
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false, // STARTTLS
-      requireTLS: true,
+      port: 465,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
