a9725487bee08504660af43adffc905122398843
William Wu <williamwuu3@gmail.com>
2025-04-18 10:32:23 -0700

    fixed hero, title, readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Hero from "@/components/Hero/Hero";
 export default function Home() {
   return (
     <>
-      <main className="flex flex-col gap-y-24">
+      <main className="flex flex-col">
         <Popup />
         <Hero />
         {/* Location Selector */}
